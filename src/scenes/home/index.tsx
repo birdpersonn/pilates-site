fc288dd803971a922bd0efcb65936038f1430524
birdpersonn <chanelcarp@gmail.com>
2023-07-16 14:45:24 -0700

    add motion to home headings

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -3,6 +3,7 @@ import { SelectedPage } from '@/shared/types'
 import HomePageText from '@/assets/HomePageText.png'
 import HomePageGraphic from '@/assets/HomePageGraphic.png'
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import { motion } from 'framer-motion';
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void;
@@ -17,15 +18,47 @@ const Home = ({ setSelectedPage }: Props) => {
             {/* MAIN HEADER */}
             <div className='z-10 mt-32 md:basis-full sm:items-center'>
                 {/* HEADINGS */}
-                <div className='md:-mt-20'>
+                <motion.div
+                    className='md:-mt-20'
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ duration: 0.6 }}
+                    variants={{
+                        hidden: {
+                            opacity: 0,
+                            x: -50
+                        },
+                        visible: {
+                            opacity: 1,
+                            x: 0
+                        }
+                    }}
+                >
                     <img src={HomePageText} alt="home-page-text" />
                     <p className='mt-8 text-sm'>
                         Discover a haven of tranquility at ClubYoga, where transformative yoga classes meet the freedom of a dedicated free practice space. Immerse yourself in our diverse range of classes led by experienced instructors, tailored to every level.
                     </p>
-                </div>
+                </motion.div>
 
                 {/* ACTIONS */}
-                <div className='mt-8 flex items-center gap-8'>
+                <motion.div
+                    className='mt-8 flex items-center gap-8'
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ delay: 0.2, duration: 0.6 }}
+                    variants={{
+                        hidden: {
+                            opacity: 0,
+                            x: -50
+                        },
+                        visible: {
+                            opacity: 1,
+                            x: 0
+                        }
+                    }}
+                >
                     <ActionButton setSelectedPage={setSelectedPage}>
                         Join Now
                     </ActionButton>
@@ -36,7 +69,7 @@ const Home = ({ setSelectedPage }: Props) => {
                     >
                         <p>Learn More!</p>
                     </AnchorLink>
-                </div>
+                </motion.div>
             </div>
 
             {/* IMAGE */}
